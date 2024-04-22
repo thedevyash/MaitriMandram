cdc02fcabb469e011671ae28a0c37a05fed1534b
thedevyash <imailsharma.yash@gmail.com>
2024-04-22 21:11:29 +0530

    Update voices.dart to remove unused import

--- lib/Screens/pages/voices.dart
+++ lib/Screens/pages/voices.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sehyogini_frontned/Screens/chatbot/chatScreen.dart';
-import 'package:sehyogini_frontned/Screens/chatbot/chatbotScreen.dart';
 
 class VoicesScreen extends StatefulWidget {
   const VoicesScreen({super.key});
